3def27eee9a4dca9d1b1adb215d1a1d51101ca9b
황주빈 <hjb06288@gmail.com>
2023-12-07 16:09:24 +0900

    feat: response dto 생성

---
+++ src/main/java/project/newscuratorservicehomeproposal/dto/responseDto/NewsDto.java
@@ -0,0 +1,26 @@
+package project.newscuratorservicehomeproposal.dto.responseDto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+import project.newscuratorservicehomeproposal.domain.News;
+
+import java.time.LocalDateTime;
+
+@Data
+public class NewsDto {
+
+    private Long id;
+    private String title;
+    private String main;
+    private String reporter;
+    private LocalDateTime createNewsDate;
+
+    public NewsDto(News news){
+        this.id = news.getId();
+        this.title = news.getTitle();
+        this.main = news.getMain();
+        this.reporter = news.getReporter();
+        this.createNewsDate = news.getCreateNewsDate();
+    }
+
+}
