90d851078ba27bfa98fcad322c331d85e6fc8724
황주빈 <hjb06288@gmail.com>
2023-12-08 14:26:38 +0900

    feat: sequence 설정

--- src/main/java/project/newscuratorservicehomeproposal/domain/News.java
+++ src/main/java/project/newscuratorservicehomeproposal/domain/News.java
@@ -1,9 +1,6 @@
 package project.newscuratorservicehomeproposal.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
@@ -11,10 +8,17 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@SequenceGenerator(
+        name = "USER_PK_GENERATOR",
+        sequenceName = "USER_PK_SEQ",
+        initialValue= 1,
+        allocationSize = 1
+)
 public class News {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator="USER_PK_GENERATOR")
     @Column(name = "news_id")
     private Long id;
 
@@ -26,6 +30,8 @@ public class News {
 
     @NotEmpty
     private String reporter;
+
+
     private LocalDateTime createNewsDate;
 
     private LocalDateTime updateNewsDate;
@@ -35,6 +41,6 @@ public class News {
     //임의 생성 방지
     protected News() {
         this.createNewsDate = LocalDateTime.now();
+        //조회수를 처음엔 0으로 초기화
     }
-
 }
